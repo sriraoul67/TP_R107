1bdd163125544852dddd9b01c6849c894fddc716
sriraoul67 <antoine.reiminger@hotmail.com>
2022-11-29 15:47:30 +0100

    test

--- TP2exo3.py
+++ TP2exo3.py
@@ -1,7 +1,29 @@
 a = 8
 b = 6
 c = 1
-a = b
-b = c
-c = a
+A = a
+
+a = c
+c = b
+b = A
 print (f'affichez {a} {b} {c}')
+
+
+a=input("Entrez la premiere  valeur : 8")
+b=input("Entrez la deuxieme  valeur : 6")
+c=input("Entrez la troisieme valeur : 1")
+
+print("Les valeurs entrees sont : a = 8" + a +", b = 6 " + b + " et c = 1" + c + b)
+print("Permutation: a ==> b, b ==> c, c ==> a")
+"""      *******************************************
+         * Completez le programme a partir d'ici.
+         *******************************************
+"""
+
+
+"""     *******************************************
+         * Ne rien modifier apres cette ligne.
+         *******************************************
+"""
+
+print("Les valeurs permutees sont : a = 1" + a + ", b = 8" + b + " et c = 6" + c)
